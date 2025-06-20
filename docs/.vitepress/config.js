f5956246eac2b5de7afa956f7d9f59e001e5c16f
Cal-ly <cal-ly@outlook.dk>
2025-06-20 10:43:33 +0200

    chore: initialize project with package.json and VitePress setup

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Snake Math',
+  description: 'Interactive Python math lessons with PyScript',
+  // Optional: customize theme, nav, sidebar here
+}
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# Welcome to Snake Math
+
+This is your in-browser Python math playground. Start by editing this page!
+
+<py-script>
+  print("Hello, PyScript!")
+</py-script>
